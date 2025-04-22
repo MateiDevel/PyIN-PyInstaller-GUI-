1791b26f62cf3fee291ea3e5f6081a08bfa4784f
mateidev <serbanmatei572@gmail.com>
2025-04-22 15:38:59 +0300

    accidentally used an old version of the file

--- pyin/__main__.py
+++ pyin/__main__.py
@@ -1,10 +1,13 @@
 import customtkinter as ctk
 from .gui import GUI
 
-app = ctk.CTk()
-app.title('PyInstaller-GUI')
-app.geometry('597x353')
-app.resizable(False, False)
-gui = GUI(master=app)
-app.mainloop()
+def main():
+    app = ctk.CTk()
+    app.title('PyInstaller-GUI')
+    app.geometry('597x353')
+    app.resizable(False, False)
+    gui = GUI(master=app)
+    app.mainloop()
 
+if __name__ == '__main__':
+    main()
